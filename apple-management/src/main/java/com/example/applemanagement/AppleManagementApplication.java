26260b177d096c8612c6b2cdbb6f180739fbdb49
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-03-15 09:39:59 +0700

    base_source

---
+++ apple-management/src/main/java/com/example/applemanagement/AppleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.applemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppleManagementApplication.class, args);
+	}
+
+}
