54ee66fd6752e0c3e6647dd49c0fde026dd24113
thanhphong8697 <thanhphong861997@gmail.com>
2023-03-18 18:20:40 +0700

    update story1

---
+++ apple-management/src/main/java/com/example/applemanagement/service/store/IStoreService.java
@@ -0,0 +1,14 @@
+package com.example.applemanagement.service.store;
+
+import com.example.applemanagement.dto.storeDTO.StoreDTO;
+import com.example.applemanagement.model.store.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface IStoreService {
+    Page<StoreDTO> listAll(Pageable pageable, String name);
+
+    Store findById(int id);
+
+    void delete(int id);
+}
