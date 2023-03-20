e4907eabbba51c8a4b221a1c1e1e8b8cef1db903
thanhphong8697 <thanhphong861997@gmail.com>
2023-03-20 09:39:14 +0700

    new update store story2

---
+++ apple-management/src/main/java/com/example/applemanagement/controller/store/RestStoreTypeController.java
@@ -0,0 +1,23 @@
+package com.example.applemanagement.controller.store;
+
+import com.example.applemanagement.dto.storeDTO.StoreTypeDTO;
+import com.example.applemanagement.service.store.IStoreTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/type")
+@CrossOrigin("*")
+public class RestStoreTypeController {
+    @Autowired
+    private IStoreTypeService iStoreTypeService;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("")
+    public List<StoreTypeDTO> listAllType(){
+        return iStoreTypeService.listAll();
+    }
+}
