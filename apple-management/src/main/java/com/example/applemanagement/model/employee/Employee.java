742cfaaa6ad3648477dd7b8d81858754c757a066
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-03-21 08:29:53 +0700

    fix_update

--- apple-management/src/main/java/com/example/applemanagement/model/employee/Employee.java
+++ apple-management/src/main/java/com/example/applemanagement/model/employee/Employee.java
@@ -27,4 +27,82 @@ public class Employee {
     @ManyToOne
     @JoinColumn(name = "position_id", referencedColumnName = "id", nullable = false)
     private Position position;
+
+    public Employee() {
+    }
+
+    public Employee(Long id, String name, String dateOfBirth, String idCard, String phoneNumber, String address, String imgUrl, Position position) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.idCard = idCard;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.imgUrl = imgUrl;
+        this.position = position;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getIdCard() {
+        return idCard;
+    }
+
+    public void setIdCard(String idCard) {
+        this.idCard = idCard;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
 }
--- apple-management/src/main/java/com/example/applemanagement/repository/employee/IPositionRepository.java
+++ apple-management/src/main/java/com/example/applemanagement/repository/employee/IPositionRepository.java
@@ -3,5 +3,5 @@ package com.example.applemanagement.repository.employee;
 import com.example.applemanagement.model.employee.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IPositionRepository extends JpaRepository<Position, Integer> {
+public interface IPositionRepository extends JpaRepository<Position, Long> {
 }
