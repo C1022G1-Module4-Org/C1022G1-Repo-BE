6d7c35ccc94263e9f70ca520d7de2c322de08a88
hoangthang1003 <110215092+hoangthang1003@users.noreply.github.com>
2023-03-21 13:32:03 +0700

    update customer

--- apple-management/src/main/java/com/example/applemanagement/service/ICustomerService.java
+++ apple-management/src/main/java/com/example/applemanagement/service/ICustomerService.java
@@ -1,16 +1,19 @@
 package com.example.applemanagement.service;
 
 
+import com.example.applemanagement.dto.customerDTO.CustomerDTO;
 import com.example.applemanagement.model.customer.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 
 public interface ICustomerService {
-    List<Customer> findAll();
+    Page<CustomerDTO> findAll(String name , Pageable pageable);
     Customer findById(long id);
     Customer findByCustomer(String name);
     void create(Customer customer);
     void delete(Customer customer);
-
+    void save(Customer customer);
 }
