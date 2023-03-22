1c7f80f5391aa25d38bd6aef51ea3a13811903e9
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-03-22 10:49:43 +0700

    Update employee story 2

--- apple-management/src/main/java/com/example/applemanagement/controller/employee/RestEmployeeController.java
+++ apple-management/src/main/java/com/example/applemanagement/controller/employee/RestEmployeeController.java
@@ -7,12 +7,18 @@ import com.example.applemanagement.service.employees.IEmployeeService;
 import com.example.applemanagement.service.employees.position.IPositionService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.data.domain.*;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/apple/employee")
@@ -33,27 +39,34 @@ public class RestEmployeeController {
         return new PageImpl<>(employees, pageable,employee.getTotalElements());
     }
 
-//    @ResponseStatus(HttpStatus.OK)
-//    @GetMapping("")
-//    public Page<Employee> EmployeeDTOPage(
-//            @Valid
-//            @PageableDefault(size = 3) Pageable pageable,
-//            @RequestParam(required = false, defaultValue = "") String name) {
-//        Pageable pageable1 = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
-//        return employeeService.findAllEmployee(name,pageable1);
-//    }
     @GetMapping("/position")
     public List<Position> findAllPosition(){
         return positionService.findAll();
     }
 
+//    @PostMapping("")
+//    @ResponseStatus(HttpStatus.CREATED)
+//    public Employee createNewEmployee(@RequestBody EmployeeDTO employeeDTO){
+//        Employee employee = new Employee();
+//        BeanUtils.copyProperties(employeeDTO,employee);
+//        return employeeService.createNewEmployee(employee);
+//    }
+
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee createNewEmployee(@RequestBody EmployeeDTO employeeDTO){
+    public ResponseEntity<?> createNewEmployeeTest(@Validated @RequestBody EmployeeDTO employeeDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            List<String> errors = bindingResult.getFieldErrors().stream()
+                    .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                    .collect(Collectors.toList());
+            return ResponseEntity.badRequest().body(errors);
+        }
+
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO,employee);
-        return employeeService.createNewEmployee(employee);
+        return ResponseEntity.ok(employeeService.createNewEmployee(employee));
     }
+
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/{id}")
     public void deleteEmployee(@PathVariable long id) {
